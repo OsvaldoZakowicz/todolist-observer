03a42d41193d079b32df6e14b857b46b49bf4e54
OsvaldoEmanuel <osvaldozakowicz@gmail.com>
2021-08-10 15:49:00 -0300

    Codigo refactorizado ordenado

--- main.js
+++ main.js
@@ -1,13 +1,74 @@
-/**
- * *El publicador (sujeto) con estado sera lalista de tareas (ul)
- * *Los observadores seran los interesados en el estado del sujeto
- */
-let todolist = []; //publicador
-let observers = [] ; //observadores
+//objeto publicador
+function Publisher() {
+  
+  /**
+   * *El publicador (Publisher) con estado sera la lista de tareas (ul)
+   * *Los observadores seran los interesados en el estado del sujeto
+   */
+  let todolist = []; //tareas del publicador
+  let observers = [] ; //observadores
 
-/**
- * *Obtenemos el formulario
- */
+  /**
+   * *Agregar una funcion a la lista de observadores
+   * La funcion agregada como parametro sera un observador
+   * @param {*} observer 
+   */
+  function addObserver(observer){
+    observers.push(observer);
+  }
+
+  /**
+   * *Notificar a cada observador
+   */
+  function notifyObservers(){
+    observers.forEach((observer)=>observer());
+  }
+
+  /**
+   * *Agregar item a la lista de tareas
+   * @param {*} item 
+   */
+  function addTodo(item){
+    todolist.push(item);
+    notifyObservers();
+  }
+
+  /**
+   * *Filtrar la lista de tareas para quitar la deseada
+   * @param {*} id 
+   */
+  function removeTodo(id){
+    todolist = todolist.filter((todo)=> todo.id !== id);
+    notifyObservers();
+  }
+
+  /**
+   * *Cargar datos provenientes de local storage en el array de tareas
+   * @param {*} savedData 
+   */
+  function loadTodos(savedData){
+    todolist = savedData;
+    notifyObservers();
+  }
+
+  function getTodoList(){
+    return todolist;
+  }
+
+  return {
+    addObserver,
+    notifyObservers,
+    addTodo,
+    removeTodo,
+    loadTodos,
+    getTodoList
+  }
+}
+
+//creamos un objeto publicador
+const publisher = Publisher();
+
+//Obtenemos el formulario
 const formulario = document.getElementById('formulario');
 
 /**
@@ -15,107 +76,48 @@ const formulario = document.getElementById('formulario');
  * y creando un objeto item, el cual sera agregado a la lista de tareas
  */
 formulario.addEventListener('submit', (e) => {
-  //evitamos el comportamiento por defecto
   e.preventDefault();
-  //obtenemos el input del formulario
   const input = formulario.elements[0];
-  //creamos un objeto de la lista de tareas, un item
   const item = {
-    //id del item, debe ser unico, usaremos la fecha
     id: Date.now(),
     descripcion: input.value,
   };
-  //agregar el item a la lista de tareas
-  addTodo(item);
-  //despejamos el input del formulario
+  publisher.addTodo(item);
   input.value = '';
 });
 
-/**
- * *Agregar item a la lista de tareas
- * @param {*} item 
- */
-function addTodo(item){
-  //agregamos a la lista un item
-  todolist.push(item);
-  //notificamos a los observadores, ya que el publicador tiene cambios
-  notifyObservers();
-}
 
 /**
  * *Mostrar tareas dentro de la lista
  * Esta funcion es un observador
  */
 function displayTodos(){
-  //obtenemos la lista desordenada
   const ul = document.querySelector('ul');
-  //limpiar constante ul
   ul.innerHTML = '';
-  //por cada elemento...
-  todolist.forEach((item) => {
-    //creamos un item html de la lista
+  publisher.getTodoList().forEach((item) => {
     const li = document.createElement('li');
-    //al item le damos la descripcion de la tarea
     li.innerText = item.descripcion;
-    //crear un boton de eliminar, y agregar este a cada li
     const button = document.createElement('button');
     button.innerText = 'Quitar';
-    //evento para quitar item de la lista
     button.addEventListener('click', ()=>{
-      removeTodo(item.id);
+      publisher.removeTodo(item.id);
     });
     li.appendChild(button);
-    //agregamos a la lista el item
     ul.appendChild(li);
   });
 }
 
-/**
- * *Agregar una funcion a la lista de observadores
- * La funcion agregada como parametro sera un observador
- * @param {*} observer 
- */
-function addObserver(observer){
-  //agregar funcion a la lista de observadores
-  observers.push(observer);
-}
-addObserver(displayTodos);
-
-/**
- * *Notificar a cada observador
- */
-function notifyObservers(){
-  observers.forEach((observer)=>observer());
-}
-
-/**
- * *Filtrar la lista de tareas para quitar la deseada
- * @param {*} id 
- */
-function removeTodo(id){
-  //uso de filter
-  todolist = todolist.filter((todo)=> todo.id !== id);
-  //notificar observadores
-  notifyObservers();
-}
+publisher.addObserver(displayTodos);
 
 /**
  * *Usamos local storage para persistir la lista de tareas
  * Esta funcion es un observador
  */
 function persistTodos(){
-  localStorage.setItem("saved-todos", JSON.stringify(todolist));
+  localStorage.setItem("saved-todos", JSON.stringify(publisher.getTodoList()));
 }
-addObserver(persistTodos);
 
-/**
- * *Cargar datos provenientes de local storage en el array de tareas
- * @param {*} savedData 
- */
-function loadTodos(savedData){
-  todolist = savedData;
-  notifyObservers();
-}
+publisher.addObserver(persistTodos);
 
 /**
  * *Evento on load, al cargar, recargar pagina, se restauran el contenido del
@@ -124,6 +126,6 @@ function loadTodos(savedData){
 window.addEventListener('load', ()=>{
   const savedData = localStorage.getItem("saved-todos");
   if (savedData) {
-    loadTodos(JSON.parse(savedData));
+    publisher.loadTodos(JSON.parse(savedData));
   }
 });
